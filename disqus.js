1b6a6acf0da2f61aec06e27a1c381e6830c690bb
fug21 <141009193+fug21@users.noreply.github.com>
2023-08-07 16:44:30 -0500

    Update and rename script.js to disqus.js

---
+++ disqus.js
@@ -0,0 +1,12 @@
+var disqus_config = function () {
+    this.page.url = 'https://fug21.github.io/'; // Cambia esta URL por la URL de tu página
+    this.page.identifier = 'unique_identifier'; // Cambia esto por un identificador único para tu página
+};
+(function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://Mercy.disqus.com/count.js'; // Cambia esto por tu subdominio de Disqus
+    s.async = true;
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
+
