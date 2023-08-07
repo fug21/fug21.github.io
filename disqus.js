18d58decfcf2e5b6006596ba7d5ca66f3477e56c
fug21 <141009193+fug21@users.noreply.github.com>
2023-08-07 16:55:26 -0500

    Update disqus.js

--- disqus.js
+++ disqus.js
@@ -4,7 +4,7 @@ var disqus_config = function () {
 };
 (function() {
     var d = document, s = d.createElement('script');
-    s.src = 'https://Mercy.disqus.com/count.js'; // Cambia esto por tu subdominio de Disqus
+    s.src = 'https://mercy.disqus.com/count.js'; // Cambia esto por tu subdominio de Disqus
     s.async = true;
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
